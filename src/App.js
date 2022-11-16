c2aaace4669bd0d546c0b7af0417ea0546d4e0f5
Fatali Fataliyev <fataliyev999@gmail.com>
2022-11-16 19:44:02 +0300

    Restaurant card,header,pagination is completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Dashboard from "./components/Dashboard";
+import Restuarants from "./components/Restuarants";
 import "./styles/App.css";
 
 
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <Dashboard/>
+      <Restuarants/>
     </>
   );
 }
